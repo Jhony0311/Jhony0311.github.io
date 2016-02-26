1608c4cd84726be295d48ca1dfd5ee0b801407f0
Jonathan Ortega <jortega0389@gmail.com>
2016-02-25 23:19:25 -0500

    adding readme, grunt new config, some dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,79 +1,68 @@
-module.exports = function (grunt) {
+module.exports = function(grunt) {
 
-  grunt.initConfig({
-    connect: {
-      server: {
-        options: {
-          hostname: '*',
-          port: 9000,
-          base: 'public/',
+    grunt.initConfig({
+        sass: {
+            compile: {
+                files: [{
+                    dest: './styles/main.css',
+                    src: './sass/main.scss'
+                }, ],
+                options: {
+                    includePaths: [
+                        'bower_components/animate.css/source/',
+                        'bower_components/foundation/scss/',
+                    ]
+                }
+            },
         },
-      }
-    },
-    open: {
-      server: {
-        path: 'localhost:9000',
-      }
-    },
-    clean: {
-      css: ['public/styles'],
-    },
-    sass: {
-      compile: {
-        files: [
-          {
-            dest: 'public/styles/main.css',
-            src: 'public/sass/main.scss'
-          },
-        ],
-        options: {
-          includePaths: [
-            'public/bower_components/animate.css/source/',
-            'public/bower_components/foundation/scss/',
-          ]
+        postcss: {
+            options: {
+                map: false,
+                processors: [
+                    require('autoprefixer')({
+                        browsers: 'last 2 version'
+                    })
+                ],
+                outputStyle: 'nested',
+            },
+            dist: {
+                src: './styles/main.css',
+                dest: './styles/main.css'
+            }
+        },
+        watch: {
+            sass: {
+                files: ['sass/*.scss'],
+                tasks: ['compile-sass']
+            },
+            js: {
+                files: ['scripts/*.js'],
+            },
+            html: {
+                files: ['*.html', '*.hbs'],
+            }
+        },
+        browserSync: {
+            bsFiles: {
+                src : [
+                    'styles/*.css',
+                    'scripts/*.js',
+                    '*.html'
+                ]
+            },
+            options: {
+                watchTask: true,
+                server: './'
+            }
         }
-      },
-    },
-    postcss: {
-      options: {
-        map: false,
-        processors: [
-          require('autoprefixer-core')({browsers: 'last 2 version'}).postcss,
-          require('csswring').postcss
-        ],
-        outputStyle: 'nested',
-      },
-      dist: {
-        src: 'public/styles/main.css',
-        dest: 'public/styles/main.css'
-      }
-    },
-    watch: {
-      sass: {
-        files: ['public/**/*.scss'],
-        tasks: ['compile-sass']
-      },
-      js: {
-        files: ['public/**/*.js'],
-      },
-      html: {
-        files: ['public/**/*.html', 'public/**/*.hbs'],
-      },
-      options: {
-        livereload: true,
-        events: ['all'],
-      }
-    },
-  });
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-postcss');
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-open');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browser-sync');
+    grunt.loadNpmTasks('grunt-postcss');
+    grunt.loadNpmTasks('grunt-sass');
 
-  grunt.registerTask('default', ['compile-sass', 'connect', 'watch']);
-  grunt.registerTask('compile-sass', ['sass', 'postcss']);
+    grunt.registerTask('default', ['compile-sass', 'browserSync', 'watch']);
+    grunt.registerTask('compile-sass', ['sass', 'postcss']);
 
 };
---
+++ README.md
@@ -0,0 +1,4 @@
+# Jonathan Ortega
+***
+## My personal website
+Small personal project to get my front end skill showcased together with my personal info.
