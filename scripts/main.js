f7c1fb25badf995c294c5142968a4e1b86827a3a
Jonathan Ortega <jortega0389@gmail.com>
2016-08-04 14:03:06 -0500

    fixing some styling issues on FF and Edge

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,22 @@
 (function() {
     'use strict';
     var headerBar = document.querySelector('#header');
+    var mainLogo =  document.querySelector('#main-logo');
+    var navLinks = document.querySelectorAll('.navigation__link');
     document.addEventListener('scroll', function(e) {
-        // document.body.scrollTop
-        console.log(document.querySelector('#header').getAttribute('height'));
-    }, true);
+        if(window.pageYOffset >= 100) {
+            headerBar.setAttribute('class', 'header header--scrolled');
+        } else {
+            headerBar.setAttribute('class', 'header');
+        }
+        // console.log(mainLogo.offsetHeight);
+    }.bind(this), true);
+
+    navLinks.forEach(function(link) {
+        link.addEventListener('click', function(e) {
+            console.log(e);
+            e.preventDefault();
+            return false;
+        })
+    })
 }());
