eb4a03f26d603f218bbacf36268976b293e05b3e
Jonathan Ortega <jortega0389@gmail.com>
2016-08-04 18:26:45 -0500

    starting migration to webpack

---
+++ scripts/app.js
@@ -0,0 +1,27 @@
+(function() {
+    'use strict';
+    var headerBar = document.querySelector('#header');
+    var mainLogo =  document.querySelector('#main-logo');
+    var navLinks = document.querySelectorAll('.navigation__link');
+    document.addEventListener('scroll', function(e) {
+        if(window.pageYOffset >= 100) {
+            headerBar.setAttribute('class', 'header header--scrolled');
+        } else {
+            headerBar.setAttribute('class', 'header');
+        }
+        // console.log(mainLogo.offsetHeight);
+    }.bind(this), true);
+
+    navLinks.forEach(function(link) {
+        link.addEventListener('click', function(e) {
+            console.log(e);
+            e.preventDefault();
+            var target = e.srcElement.hash;
+            // jump(target, {
+            //     duration: 500,
+            //     offset: headerBar.getAttribute('height')
+            // });
+            return false;
+        })
+    }.bind(this));
+}());
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    entry: "./scripts/app.js",
+    output: {
+        filename: "./scripts/main.js"
+    },
+    module: {
+        loaders: [{
+            test: /\.es6$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['react', 'es2015']
+            }
+        }]
+    },
+    resolve: {
+        extensions: ['', '.js', '.es6']
+    }
+}
