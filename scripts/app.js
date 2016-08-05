cdaaea7e63508dce866216f51b19bc18ece71234
Jonathan Ortega <jortega0389@gmail.com>
2016-08-05 15:21:03 -0500

    some major updates on dev environment

--- scripts/app.js
+++ scripts/app.js
@@ -1,27 +1,27 @@
-(function() {
-    'use strict';
-    var headerBar = document.querySelector('#header');
-    var mainLogo =  document.querySelector('#main-logo');
-    var navLinks = document.querySelectorAll('.navigation__link');
-    document.addEventListener('scroll', function(e) {
-        if(window.pageYOffset >= 100) {
-            headerBar.setAttribute('class', 'header header--scrolled');
-        } else {
-            headerBar.setAttribute('class', 'header');
-        }
-        // console.log(mainLogo.offsetHeight);
-    }.bind(this), true);
+'use strict';
+import mainStyle from '../sass/main.scss';
+// import jump from 'jump.js';
+let mainLogo =  document.querySelector('#main-logo');
+let headerBar = document.querySelector('#header');
+let navLinks = document.querySelectorAll('.navigation__link');
+document.addEventListener('scroll', function(e) {
+    if(window.pageYOffset >= 100) {
+        headerBar.setAttribute('class', 'header header--scrolled');
+    } else {
+        headerBar.setAttribute('class', 'header');
+    }
+    // console.log(mainLogo.offsetHeight);
+}.bind(this), true);
 
-    navLinks.forEach(function(link) {
-        link.addEventListener('click', function(e) {
-            console.log(e);
-            e.preventDefault();
-            var target = e.srcElement.hash;
-            // jump(target, {
-            //     duration: 500,
-            //     offset: headerBar.getAttribute('height')
-            // });
-            return false;
-        })
-    }.bind(this));
-}());
+navLinks.forEach(function(link) {
+    link.addEventListener('click', function(e) {
+        console.log(e);
+        e.preventDefault();
+        let target = e.srcElement.hash;
+        // jump(target, {
+        //     duration: 500,
+        //     offset: headerBar.getAttribute('height')
+        // });
+        return false;
+    })
+}.bind(this));
